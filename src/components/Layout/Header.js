d9c2d02c61c5b40c860dc9685547500a0f68fc3c
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2022-12-29 22:43:41 -0500

    cart button and icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,13 @@ import { Fragment } from "react";
 
 import image from '../../assets/scoobert.jpg';
 import classes from './Header.module.css';
-
+import HeaderCartButton from './HeaderCartButton';
 const Header = props => {
     return (
     <Fragment>
         <header className={classes.header}>
             <h1>Opium Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={image} alt="Scoobert"/>
